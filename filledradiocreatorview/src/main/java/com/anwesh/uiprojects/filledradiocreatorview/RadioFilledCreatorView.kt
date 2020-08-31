86fdfac345025f6ee8a184fb98daef45303dd118
Anwesh43 <anweshthecool0@gmail.com>
2020-08-31 14:26:44 +0530

    adding constants and libraries

--- filledradiocreatorview/src/main/java/com/anwesh/uiprojects/filledradiocreatorview/RadioFilledCreatorView.kt
+++ filledradiocreatorview/src/main/java/com/anwesh/uiprojects/filledradiocreatorview/RadioFilledCreatorView.kt
@@ -4,3 +4,26 @@ package com.anwesh.uiprojects.filledradiocreatorview
  * Created by anweshmishra on 31/08/20.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#FFC107",
+        "#3F51B5",
+        "#4CAF50",
+        "#2196F3"
+).map{Color.parseColor(it)}.toTypedArray()
+val parts : Int = 2
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.8f
+val concFactor : Float = 7.2f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#bdbdbd")
